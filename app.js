06347bfe6b3866120f860f3806d5e545438f0ba7
felipecaos1 <andresfelipecao@gmail.com>
2022-02-05 11:45:20 -0500

    contador

--- app.js
+++ app.js
@@ -10,5 +10,72 @@ const reloj=()=>{
     
 
 }
+function startCount(){
 
-setInterval(reloj, 1000);
+    const intervalo= setInterval(()=>{
+        console.log("entro");
+        let segundos=parseInt(document.getElementById("segundos").innerHTML,10);
+    let minutos=parseInt(document.getElementById("minutes").innerHTML,10);
+    let horas=parseInt(document.getElementById("hours").innerHTML,10);
+    let dias=parseInt(document.getElementById("days").innerHTML,10);
+    
+    if(segundos==0){
+        if(dias!=0 || horas!=0 || minutos!=0){
+            document.getElementById("segundos").innerHTML=59;
+            if(minutos==0){
+                if(dias!=0 || horas!=0){
+    
+                    document.getElementById("minutes").innerHTML=59;
+                    if(horas==0){
+                        if(dias!=0){
+        
+                            document.getElementById("hours").innerHTML=24;
+                            if(dias==0){}
+                            else{
+                                document.getElementById("days").innerHTML=formato(dias-1);
+                            }
+                        }
+                        else{
+        
+                            
+                        }
+                    }
+                    else{
+                        
+                        document.getElementById("hours").innerHTML=formato(horas-1);
+                    }
+                }
+                else{
+                    
+                }
+                
+            }
+            else{
+                document.getElementById("minutes").innerHTML=formato(minutos-1);
+            }
+        }
+        else{
+            
+            
+        }
+        
+       
+    }
+    else{
+        document.getElementById("segundos").innerHTML=formato(segundos-1);
+    }
+   
+
+    if(dias==0&&horas==0&&minutos==0&&segundos==0){
+        clearInterval(intervalo);
+    }
+    },100)
+}
+
+function formato(x){
+    if(x<10){
+        x= '0'+x;
+    }
+    return x
+}
+
